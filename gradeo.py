61070d6c3ea1623aedaec166022a9d872b6c53e2
Joshua D Katz <gravypod@gravypod.com>
2015-12-13 23:48:09 -0500

    Start GradeO
    The automated grader.

---
+++ gradeo.py
@@ -0,0 +1,8 @@
+__author__ = 'Joshua D. Katz'
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ loader.py
@@ -0,0 +1,33 @@
+from os.path import isfile, basename, dirname, relpath
+from traceback import format_exc
+
+
+__author__ = 'Joshua D. Katz'
+
+
+def load_module(code_path):
+    """Load a module from it's path. The code_path is a path to the file."""
+    if not isfile(code_path):
+        return None
+
+    try:
+
+        # Magic module loading
+        code_dir = relpath(dirname(code_path))
+        code_file = basename(code_path)
+
+        module_name = code_file[0:code_file.index(".")]
+
+        module = __import__("%s.%s" % (code_dir, module_name), fromlist=["*"])
+
+        return module
+
+    # Must handle ANY exception.
+    # This will come from the module we are loading.
+    # Any exception thrown is from the student's code.
+    except BaseException:
+
+        print("%s failed to load" % code_path)
+        # traceback.format_exec() returns a string.
+        # The string is the text that the exception would have been thrown.
+        return format_exc()
