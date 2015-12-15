a95c792820c0f8289201c639126238fecec9042e
Joshua D Katz <gravypod@gravypod.com>
2015-12-14 20:03:30 -0500

    Document grade_labs and validate command line input

--- gradeo.py
+++ gradeo.py
@@ -2,14 +2,34 @@ import loader
 from traceback import format_exc
 from report_card import print_incorrect_box, print_lab_score
 import argparse
+from os.path import isfile, isdir, exists
 
 __author__ = 'Joshua D. Katz'
 
 
+def is_file(path):
+    if exists(path) and isfile(path):
+        return path
+    raise argparse.ArgumentTypeError("%s is not an existing file" % path)
+
+
+def is_folder(path):
+    if exists(path) and isdir(path):
+        return path
+    raise argparse.ArgumentTypeError("%s is not an existing folder" % path)
+
+
 def grade_labs(grader_file, lab_submission_path):
+    """
+        Grade labs and return a dictionary of UCIDs and score results.
+        The score result WILL BE A STRING if it files to load.
+
+        grader_file is the path to the grader file.
+        lab_submission_path is a path to a folder containing labs.
+    """
     try:
         auto_grader = loader.load_grader(grader_file)
-        print("Grader loaded from section %s and lab number %0.3d" % (auto_grader.class_section, auto_grader.lab_number))
+        print("Grader loaded: section %s and lab number %0.3d" % (auto_grader.class_section, auto_grader.lab_number))
     except:
         print("Error loading AutoGrader file.")
         print(format_exc())
@@ -43,8 +63,8 @@ def grade(grader_file, lab_submission_path, short_print):
 
 def main():
     parser = argparse.ArgumentParser(description="GradeO automatic lab grader")
-    parser.add_argument("--grader", action="store", type=str, help="The AutoGrader file to use", required=True)
-    parser.add_argument("--labs", action="store", type=str, help="Folder with labs to grade", default="labs/")
+    parser.add_argument("--grader", action="store", type=is_file, help="The AutoGrader file to use", required=True)
+    parser.add_argument("--labs", action="store", type=is_folder, help="Folder with labs to grade", default="labs/")
     parser.add_argument("--short_print", action="store_true", help="Folder with labs to grade", default=False)
     options = parser.parse_args()
 
